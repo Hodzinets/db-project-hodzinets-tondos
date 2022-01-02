8ff48a2af54c623f320908af4b8772f0028cf481
Hodzinets <hodzinets@student.agh.edu.pl>
2022-01-02 15:47:02 +0100

    Added Suppliers table

---
+++ app/src/main/java/com/db/app/database/model/Supplier.java
@@ -0,0 +1,51 @@
+package com.db.app.database.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Suppliers")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SupplierID")
+    private Long id;
+
+    @Column(name = "CompanyName")
+    private String companyName;
+
+    @Column(name = "ContactName")
+    private String contactName;
+
+    @Column(name = "ContactTitle")
+    private String contactTitle;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "City")
+    private String city;
+
+    @Column(name = "Region")
+    private String region;
+
+    @Column(name = "PostalCode")
+    private String postalCode;
+
+    @Column(name = "Country")
+    private String country;
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "Fax")
+    private String fax;
+
+    @Column(name = "HomePage")
+    private String homePage;
+}
